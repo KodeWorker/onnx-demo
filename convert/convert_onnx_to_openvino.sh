69729317f1a1a66324f4d2f6d9693362760c7e15
KodeWorker <fxp61005@gmail.com>
2020-04-27 17:49:02 +0800

    20200427 update

--- convert/convert_onnx_to_openvino.sh
+++ convert/convert_onnx_to_openvino.sh
@@ -4,11 +4,18 @@ CONDA_SETTING_SCRIPT="${CONDA_PATH}/../../etc/profile.d/conda.sh"
 source "${CONDA_SETTING_SCRIPT}"
 conda activate openvino
 
-MODEL_NAME="efficientnet-b0"
+MODEL_NAME="efficientnet-b7"
 INPUT_MODEL="opt-"$MODEL_NAME".onnx"
+OUTPUT_DIR=$MODEL_NAME
+
+rm -r -f $MODEL_NAME
+mkdir $OUTPUT_DIR
 
 python "C:\Program Files (x86)\IntelSWTools\openvino\deployment_tools\model_optimizer\mo.py" \
---input_shape=[1,160,160,3] \
+--input_shape=[1,3,224,224] \
 --data_type=FP16 \
+--mean_values "[123.675, 116.28, 103.53]" \
+--scale_values "[58.395, 57.12, 57.375]" \
+--reverse_input_channels \
 --input_model $INPUT_MODEL \
---output_dir openvino_model
+--output_dir $OUTPUT_DIR
